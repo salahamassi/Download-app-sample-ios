9bbee550b916be31d50b63b80b2a368cefdb94f7
salahamassi <salahelamassi@gmail.com>
2020-01-01 02:00:38 +0200

    first commit

---
+++ Download App/Helper/Extensions/UInt64+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UInt64+Extensions.swift
+//  Download App
+//
+//  Created by Salah Amassi on 1/1/20.
+//  Copyright © 2020 Salah Amassi. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# Download-app-sample-ios
